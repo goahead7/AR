e748cea84ba626a3dfad0a1cd03a39a6716c2d5d
Sophia <sophia.kuznetsova.7@gmail.com>
2021-06-23 07:55:53 +1000

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,24 +1,11 @@
 <?php
-function connect(): PDO
-{
+function connect(): PDO {
     $host='localhost';
     $db = 'db';
     $user = 'root';
     $password ='**********';
     try {
-        $dsn = "mysql:host=$host;port=3306;dbname=$db;";
-
-        return new PDO(
-            $dsn,
-            $user,
-            $password,
-            [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
-        );
+        $dbh = new PDO ('mysql:host=$host:port=3306;dbname=$db',$user,$password);
     } catch (PDOException $e) {
         die($e->getMessage());
     }
-    finally {
-        echo 'ok';
-    }
-}
-return connect();
